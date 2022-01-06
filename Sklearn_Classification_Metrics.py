3e2961d635aaa7250c0fc0b94bcd739d431b13f5
Nick Carrick <57917237+nicholascarrick@users.noreply.github.com>
2022-01-05 19:57:53 -0800

    Update Sklearn_Classification_Metrics.py

--- Sklearn_Classification_Metrics.py
+++ Sklearn_Classification_Metrics.py
@@ -1,4 +1,4 @@
-import sklearn.metrics
+from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score,confusion_matrix
 
 def evaluate_model(y_true,y_pred):
   metrics_dict = {}
@@ -6,13 +6,13 @@ def evaluate_model(y_true,y_pred):
   accuracy = round((accuracy_score(y_true,y_pred))*100,2)
   metrics_dict["accuracy"] = str(accuracy) + "%"
 
-  precision = round((precision_score(y_true,y_pred))*100,2)
+  precision = round((precision_score(y_true,y_pred,average="micro"))*100,2)
   metrics_dict["precision"] = str(precision) + "%"
 
-  recall = round((recall_score(y_true,y_pred))*100,2)
+  recall = round((recall_score(y_true,y_pred,average="micro"))*100,2)
   metrics_dict["recall"] = str(recall) + "%"
 
-  f1 = round((f1_score(y_true,y_pred))*100,2)
+  f1 = round((f1_score(y_true,y_pred,average="micro"))*100,2)
   metrics_dict["f1_score"] = str(f1) + "%"
 
   confuse = confusion_matrix(y_true,y_pred)
